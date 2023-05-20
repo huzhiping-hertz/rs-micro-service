5e72ebb37f9abceb1f8a5536a8d9a6617e28b86d
13810311272 <huzhiping001@126.com>
2023-05-20 23:58:39 +0800

    update

--- README.md
+++ README.md
@@ -1,13 +1,26 @@
-0.Install Python3 and pip
+0. Install Python3 and pip
 
-1.pip install conan
-  关于Linux下找不到conan命令
-  运行source ~/.profile
-  然后执行conan profile detect
-  会生成/home/hz/.conan2/profiles/default 文件
-  修改 compiler.cppstd=gnu17，部分软件需要c++17
-  注意：用到了drogon_ctl,生成view用，所有将drogon_ctl拷贝到了
-  /usr/local/bin下,drogon_ctl 是conanfile.py中配置生成
+1. pip install conan
+
+2. 安装cmake
+    Linux 安装
+    windows 下安装后需要设置下环境变量
+    windows下vs提供了cmake，如果安装的话会冲突，导致编译出问题
+    例如：C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin\
+
+2. conan profile detect
+
+   linux下会生成: /home/hz/.conan2/profiles/default 文件
+   windows下会生成: C:\Users\Administrator\.conan2\profiles\default
+   关于Linux下找不到conan命令,运行source ~/.profile
+
+   修改 profiles\default 文件
+   compiler.cppstd=gnu17，部分软件需要c++17
+
+   注意：Linux 下用到了drogon_ctl,生成view用，所有将drogon_ctl拷贝到了 /usr/local/bin下,drogon_ctl 是conanfile.py中配置生成
+        Windows 下用到drogon_ctl,,生成view用，所有将drogon_ctl拷贝到了 c:/work/drogon/下,drogon_ctl 是conanfile.py中配置生成
+
+   其实：drogon 生成view 有些不太合适目前的项目，只需要生产api就可以，所以可以删除drogon_ctl 的依赖
 
 2.cd rm-api-service
 
@@ -19,4 +32,7 @@ Release模式
 Debug模式 
 5.conan install . -s build_type=Debug --build=missing
 
+
+编译器IDE目前都是用的vscode
+windows 下用vs2022 编译有问题
 
--- WebApi/controllers/LteCtl.cpp
+++ WebApi/controllers/LteCtl.cpp
@@ -1,7 +1,5 @@
 #include "LteCtl.h"
-#include  <Poco/Thread.h>
 #include "DomainModel/LteDemodulation.h"
-#include "DomainModel/LteFreqAnalysisIBE.h"
 
 void LteCtl::GetFreqAnalysis(const HttpRequestPtr &req,std::function<void (const HttpResponsePtr &)> &&callback)
 {
@@ -39,39 +37,4 @@ void LteCtl::FreqAnalysis(const HttpRequestPtr &req,std::function<void (const Ht
     auto resp = HttpResponse::newHttpJsonResponse(rsObj);
     callback(resp);
 
-}
-
-void LteCtl::GetDemodulate(const HttpRequestPtr &req,std::function<void (const HttpResponsePtr &)> &&callback)
-{
-
-    LteFreqAnalysisIBE ltefreq;
-    string params=ltefreq.getParams();
-
-    Json::Value paramObj;
-    JSONCPP_STRING err;
-    const auto rawJsonLength = static_cast<int>(params.length());
-    Json::CharReaderBuilder builder;
-    const std::unique_ptr<Json::CharReader> reader(builder.newCharReader());
-    if (!reader->parse(params.c_str(), params.c_str() + rawJsonLength, &paramObj,&err)) {
-        paramObj["error"]="params template parse error";
-    }
-    auto resp = HttpResponse::newHttpJsonResponse(paramObj);
-    callback(resp);
-}
-
-void LteCtl::Demodulate(const HttpRequestPtr &req,std::function<void (const HttpResponsePtr &)> &&callback)
-{
-    auto paramObj = req->getJsonObject();
-
-    Json::StreamWriterBuilder builder;
-    const std::string paramStr= Json::writeString(builder, *paramObj);
-
-    LteFreqAnalysisIBE ltefreq;
-    string rs=ltefreq.run(paramStr);
-
-
-    Json::Value rsObj;
-    rsObj["result"] = rs;
-    auto resp = HttpResponse::newHttpJsonResponse(rsObj);
-    callback(resp);
 }
